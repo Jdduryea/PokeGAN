e2289b4a85929f7632c6f81311151658ecf0f712
smahesh1 <smahesh3288@gmail.com>
2017-08-28 20:41:02 -0400

    Modified image_process to make alpha pixels turn to white and processed a couple generations

--- image_process.py
+++ image_process.py
@@ -4,27 +4,41 @@ import numpy as np
 import os, sys
 
 def process(dirs, path):
-	"""
-	Takes all the png images in the chosen directories, resizes them to 64 x 64 
-	and converts the images to RGB
-	"""
+    """
+    Takes all the png images in the chosen directories, resizes them to 64 x 64 
+    and converts the images to RGB
+    """
     for image_file in dirs:
         if os.path.isfile(path + image_file):
-        	im = Image.open(path+item)
-        	f, e = os.path.splitext(path + image_file)
+            im = Image.open(path+image_file)
+            f, e = os.path.splitext(path + image_file) 
 
-        	#Resize image
-        	imResize = im.resize((64,64), Image.ANTIALIAS)
+            #Convert to RGBA to use as a mask 
+            rgba_im = im.convert('RGBA')
 
-        	#Convert to RGB
-        	rgb_im = imResize.convert('RGB')
-    		#pic = np.asarray(rgb_im)
-        	rgb_im.save(f + "_resized.png", 'PNG', quality=90)
+            # White colour (r,g,b,a)
+            whitened = Image.new('RGBA', rgba_im.size, (255,255,255,255)) 
+
+            #Paste rgba_im on top of a new image to mask alpha channel and effectively whiten it
+            whitened.paste(rgba_im, mask=rgba_im) 
+
+            #Resize image
+            imResize = whitened.resize((64,64), Image.ANTIALIAS)
+
+
+            imResize.save(f + "_resized.png", 'PNG', quality=90)
 
 #Example Path "C:\Users\user\PokeGAN\Images\Generation 5\\black-white\\"
 
+def delete(dirs,path):
+    for image_file in dirs:
+        if "resized" in (path + image_file) and os.path.isfile(path + image_file):
+            os.remove(path + image_file)
+
+
 path = ""
 directories = os.listdir( path )
-print(directories)
 
-resize(directories, path)
+process(directories, path)
+
+#delete(directories, path)
