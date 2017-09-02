212ba981677f89773cc219cdc244486d3c23a2dc
Jack Duryea <Jdduryea@users.noreply.github.com>
2017-09-01 19:42:52 -0500

    added license and authorship info

--- dcgan.py
+++ dcgan.py
@@ -1,3 +1,29 @@
+"""
+Written by Jack Duryea (jd50@rice.edu) and Shashank Mahesh (sm103@rice.edu)
+Rice University
+Department of Computer Science
+
+MIT License
+Copyright (c) 2017 Jack Duryea
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Reshape
@@ -13,13 +39,6 @@ from PIL import Image
 import argparse
 import math
 
-"""
-TODO:
-1. Read through code, understand and add comments
-2. Check architecture of models
-3. Image processesing
-4. Fine tune parameters, notably, change the learning rates, shouldn't be the same
-"""
 
 ## Deep generative model
 def generator_model():
